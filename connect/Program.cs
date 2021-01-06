5466657e507331a42720d03cb57aea2549ccd3f3
Дмитрий Васильев <d.vasilyev@uni-eng.ru>
2021-01-06 12:14:46 +0300

    Ферстный камит

---
+++ connect/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delMeNav
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Process.Start("cmd.exe", "/c rasdial NAMAE_CONN MOE_NAMAE MOE_PASSOWODE");
+        }
+    }
+}
---
+++ desconekt/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace desconekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Process.Start("cmd.exe", "/c rasdial /d");
+        }
+    }
+}
